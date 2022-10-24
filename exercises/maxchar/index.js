2ff565988c41c1dc676c79be8d1b36e4fa0cf3e2
Yeugen1 <yeugen@gmail.com>
2022-10-24 21:56:29 +0200

    max chars

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const hashMap = {}
+    let max = 0
+    let maxChar = ''
 
-module.exports = maxChar;
+    for (const char of str) {
+        if (!hashMap[char]) hashMap[char] = 1
+        else hashMap[char]++
+    }
+
+    for (const char in hashMap) {
+        if (hashMap[char] > max) {
+            max = hashMap[char]
+            maxChar = char
+        }
+    }
+
+    return maxChar
+}
+
+module.exports = maxChar
