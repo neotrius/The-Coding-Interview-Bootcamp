30b9cbf125cc2f1ab65561b7004076a8bf281291
Yeugen1 <yeugen@gmail.com>
2022-10-26 15:14:50 +0200

    vowels aeiou

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,18 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    const matches = str.match(/[aeiou]/ig) || null
+    return matches ? matches.length : 0
+}
 
-module.exports = vowels;
+// function vowels(str) {
+//     let counter = 0
+//     const check = ['a', 'e', 'i', 'o', 'u']
+//     for (const el of str.toLowerCase()) {
+//         if (check.includes(el)) counter++
+//     }
+//     return counter
+// }
+
+module.exports = vowels
